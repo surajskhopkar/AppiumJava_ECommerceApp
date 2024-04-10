7dd0b3653c371eebcdfcef0c8eceee75db04be3b
surajskhopkar <suraj.khopkar@icloud.com>
2024-04-10 23:47:54 +0530

    Updated Test Case 3

--- src/test/java/TestCases/TestCase3.java
+++ src/test/java/TestCases/TestCase3.java
@@ -4,9 +4,12 @@ package TestCases;
 import Utils.Configuration;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TestCase3 extends Configuration {
 
+    @Test
     public void AddProduct() throws InterruptedException {
         driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Robert Dwayne");
         driver.hideKeyboard();
@@ -16,7 +19,29 @@ public class TestCase3 extends Configuration {
         driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='India']")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
         Thread.sleep(5000);
+
+        //Scroll to product
         driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"))"));
+        int productCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
+        System.out.print(productCount);
+
+        for(int i=0; i<productCount; i++){
+            String productName = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
+            if (productName.equalsIgnoreCase("Jordan 6 Rings")){
+                driver.findElement(By.id("com.androidsample.generalstore:id/productAddCart")).click();
+            }
+        }
+        Thread.sleep(5000);
+
+        //Navigate to cart
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+        Thread.sleep(3000);
+
+        //Verify if the product is added in the cart
+        String cartProduct = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
+        Assert.assertEquals(cartProduct, "Jordan 6 Rings");
+
+
 
 
     }
