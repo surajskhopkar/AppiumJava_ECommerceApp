8bdd4b53ce146876e341155434aca264118a242b
surajskhopkar <suraj.khopkar@icloud.com>
2024-04-11 10:33:23 +0530

    Updated Test Case 3

--- src/test/java/TestCases/TestCase3.java
+++ src/test/java/TestCases/TestCase3.java
@@ -4,8 +4,13 @@ package TestCases;
 import Utils.Configuration;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class TestCase3 extends Configuration {
 
@@ -31,11 +36,15 @@ public class TestCase3 extends Configuration {
                 driver.findElement(By.id("com.androidsample.generalstore:id/productAddCart")).click();
             }
         }
-        Thread.sleep(5000);
 
+        Thread.sleep(3000);
         //Navigate to cart
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
-        Thread.sleep(3000);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains
+                (driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")),"text", "Cart"));
+
 
         //Verify if the product is added in the cart
         String cartProduct = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
